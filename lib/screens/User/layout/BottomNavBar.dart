91e563e983fa5ee88e7d77bd1dd74a30bd719173
neta <netaaulya@gmail.com>
2025-08-13 11:24:21 +0700

    history

--- lib/screens/User/layout/BottomNavBar.dart
+++ lib/screens/User/layout/BottomNavBar.dart
@@ -5,6 +5,7 @@ import 'package:iconsax/iconsax.dart';
 import '../home/PesananSaya.dart';
 import '../home/Dashboard.dart';
 import '../home/CekProses.dart';
+import '../home/History.dart';
 
 class BottomNavBar extends StatelessWidget {
   final int selectedIndex;
@@ -39,8 +40,21 @@ class BottomNavBar extends StatelessWidget {
             children: [
               _buildNavItem(context, Iconsax.home, "Home", 0, const Dashboard()),
               _buildNavItem(context, Iconsax.shopping_cart, "Order", 1, const CekProses()),
-              // _buildNavItem(context, Iconsax.wallet, "History", 2, const DetailPesanan()),
-              _buildNavItem(context, Iconsax.notification, "Notification", 3, const Placeholder()),
+              _buildNavItem(
+                context,
+                Iconsax.wallet, // bisa ganti jadi icon lain misal Icons.history
+                "History",
+                2,
+                HistoryScreen(
+                  onItemSelected: (index) {
+                    // kalau mau balik ke dashboard pakai replace biar nggak numpuk route
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (_) => const Dashboard()),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
